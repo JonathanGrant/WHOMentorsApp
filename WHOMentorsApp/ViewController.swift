0c52fbaaa12c18b0f0b49bed5a073b7c8d4bccc4
Jonathan Allen Grant <jonathanallengrant@gmail.com>
2015-07-17 20:34:32 -0700

    Initial Commit

---
+++ WHOMentorsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WHOMentorsApp
+//
+//  Created by Jonathan Grant on 7/17/15.
+//  Copyright © 2015 Jonathan Grant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
